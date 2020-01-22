7288948c2aabfb69084488314d5b2d73431afefc
Justine <justine.dejong@student.uva.nl>
2020-01-22 09:52:38 +0000

    db queries voor game

--- application.py
+++ application.py
@@ -46,10 +46,11 @@ def index():
 
         # Haalt alle dieren uit categorie op en selecteerd 10 voor spel
         animalrows = db.execute("SELECT animal, unsplash FROM animals WHERE domain = :domain", domain=level)
-        quiz = random.sample(animalrows, 3)
+        quiz = random.sample(animalrows, 5)
         print(quiz)
         # Slaat huidige game data op
         session["game_data"] = {"round_number": 1, "rounds": quiz}
+
 
         return redirect("search")
 
@@ -91,6 +92,7 @@ def nickname():
             return ("Nickname has to be at least 1 character long")
 
         result = db.execute("INSERT INTO Users (username) VALUES (:name)", name=nickname)
+        session["nickname"] = nickname
 
         if not result:
             return ("Nickname already in use")
@@ -123,6 +125,7 @@ def search():
 
 @app.route("/question", methods=["GET", "POST"])
 def question():
+
     if request.method == "GET":
 
         # Kiest eerste dier uit rij voor game display
@@ -139,25 +142,46 @@ def question():
 
     if request.method == "POST":
 
+        # als je bij de laatste vraag bent dan
         if len(session["game_data"]["rounds"]) == 1:
-            return render_template("test.html", answer="YOU WON THE GAME, or not")
+
+            # wordt de gespeelde game uit de db gehaald
+            db.execute("DELETE FROM game WHERE nickname = :nickname",
+                       nickname=session["nickname"])
+
+            return render_template("winner.html", answer="YOU WON THE GAME, or not")
 
         # Session data ophalen
         animalname = session["game_data"]["rounds"][0]["animal"]
         unsplashanimal = session["game_data"]["rounds"][0]["unsplash"]
-
+        round_number = session["game_data"]["round_number"]
+        nickname = session["nickname"]
 
         # Antwoord van gebruiker ophalen
         user_input = ""
         for letter in range(len(animalname)):
             user_input += request.form.get("box" + str(letter))
 
-        # Valideert antwoord TOEVOEGEN STUK JUSTINE
-        # En score toevoegen, TOEVOEGEN STUK JUSTINE
+        #checks if answer is correct
         if animalname == user_input.lower():
             answer = True
         else:
             answer = False
+
+        # check's users gamestatus
+        statusrow = db.execute("SELECT status FROM game WHERE nickname = :nickname", nickname=nickname)
+
+        #if user just started the game
+        if len(statusrow) == 0:
+
+            # insert nickname and status in db
+            db.execute("INSERT INTO game (nickname, status) VALUES (:nickname, :status )", nickname=nickname, status=str(int(answer)))
+        else:
+            # previous answers + new answer
+            status = str(statusrow[0]["status"]) + str(int(answer))
+
+            # updates the status with new answer
+            db.execute("UPDATE game SET status = :status WHERE nickname = :nickname", status=status, nickname=nickname)
 
         # Volgende vraag opzetten
         session["game_data"]["rounds"].remove(session["game_data"]["rounds"][0])
