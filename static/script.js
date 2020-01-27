f97a6be1b80467bc2aaacad076916307a2bb96df
Tijmen Kort <tijmen.kort@student.uva.nl>
2020-01-27 14:36:47 +0000

    backspace functie in question.html

--- static/script.js
+++ static/script.js
@@ -1,6 +1,12 @@
 // Auto tab functie voor question.html
 function autotab(current,to){
-    if (current.getAttribute && current.value.length==current.getAttribute("maxlength")) {
+    if (current.value.length==1) {
+        to.focus()
+        }
+}
+
+function backspace(current,to){
+    if (current.value.length == 0 && event.code == "Backspace") {
         to.focus()
         }
 }
